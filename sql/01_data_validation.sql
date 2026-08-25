1810e80543e8edc921cc96757d586ad206346db2
MaraOKeeffe <okeeffe@havenphysicaltherapy.org>
2026-08-25 12:18:28 -0600

    Revise dataset validation query description
    Updated the validation query description for clarity and accuracy regarding null values in denial reason codes.

--- sql/01_data_validation.sql
+++ sql/01_data_validation.sql
@@ -1,4 +1,4 @@
-The dataset had null values for many claims that did not have a denial code. To be sure that all null values were considered accepted claims and that all denied claims had a denial code, I ran this query before completing other analysis.
+The dataset contained null values in the denial reason code field. Before using denial reason code in later analyses, I verified how null and non-null denial reason codes appeared within claims outcomes. The validation showed that null denial reason codes occurred only among paid, partially paid, and pending claims while all 33,664 claims with non-null denial reason codes had an outcome of denied. This confirmed that denial reason code presence could be used as a proxy for denial claims in this dataset
   SELECT
   outcome,
   COUNTIF(denial_reason_code IS NULL) AS null_reason_code,
